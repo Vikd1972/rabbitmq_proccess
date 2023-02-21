124cc3acfa75f1875af72c81fed0ff80c646276e
Viktor Dmitriev <vikdmitriev72@gmail.com>
2023-02-21 17:37:08 +0300

    migrating the publisher to a separate server

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,7 @@
-import sendMessge from './publisher/send';
 import receiveMessage from './subscriber/receive';
 
 (async () => {
   try {
-    sendMessge();
-    receiveMessage();
     receiveMessage();
   } catch (error) {
     console.log(error);
--- src/subscriber/receive.ts
+++ src/subscriber/receive.ts
@@ -1,40 +1,40 @@
 /* eslint-disable no-console */
 import amqp from 'amqplib/callback_api';
 
-// const receiveMessage = () => {
-amqp.connect('amqp://localhost', (error0, connection) => {
-  if (error0) {
-    throw error0;
-  }
-  connection.createChannel((error1, channel) => {
-    if (error1) {
-      throw error1;
+const receiveMessage = () => {
+  amqp.connect('amqp://localhost', (error0, connection) => {
+    if (error0) {
+      throw error0;
     }
-    const exchange = 'logs';
-
-    channel.assertExchange(exchange, 'fanout', {
-      durable: false,
-    });
-
-    channel.assertQueue('', {
-      exclusive: true,
-    }, (error2, q) => {
-      if (error2) {
-        throw error2;
+    connection.createChannel((error1, channel) => {
+      if (error1) {
+        throw error1;
       }
-      console.log(' [*] Waiting for messages in %s. To exit press CTRL+C', q.queue);
-      channel.bindQueue(q.queue, exchange, '');
+      const exchange = 'logs';
 
-      channel.consume(q.queue, (msg) => {
-        if (msg.content) {
-          console.log(' [x] %s', msg.content.toString());
+      channel.assertExchange(exchange, 'fanout', {
+        durable: false,
+      });
+
+      channel.assertQueue('', {
+        exclusive: true,
+      }, (error2, q) => {
+        if (error2) {
+          throw error2;
         }
-      }, {
-        noAck: true,
+        console.log(' [*] Waiting for messages in %s. To exit press CTRL+C', q.queue);
+        channel.bindQueue(q.queue, exchange, '');
+
+        channel.consume(q.queue, (msg) => {
+          if (msg.content) {
+            console.log(' [x] %s', msg.content.toString());
+          }
+        }, {
+          noAck: true,
+        });
       });
     });
   });
-});
-// };
+};
 
-// export default receiveMessage;
+export default receiveMessage;
